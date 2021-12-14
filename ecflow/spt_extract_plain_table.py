8b6c27b3cd94b378c5d2f1e449bc8304f38232db
JOgden99 <josh.ogden.1999@gmail.com>
2021-12-14 12:54:15 -0700

    Revise code to use a single loop to interpolate, add values to a pandas dataframe and write to file all at once

--- ecflow/spt_extract_plain_table.py
+++ ecflow/spt_extract_plain_table.py
@@ -1,7 +1,12 @@
 #################################################################
 #
 # File: spt_extract_plain_table.py
-# Author(s): Michael Souffront, Wade Roberts, Spencer McDonald
+# Author(s):
+#   Michael Souffront,
+#   Wade Roberts,
+#   Spencer McDonald,
+#   Josh Ogden,
+#   Riley Hales
 # Date: 03/07/2018
 # Last Updated: 05/28/2020
 # Purpose: Calculate basic statistics for GloFAS-RAPID files and
@@ -70,102 +75,123 @@ def extract_summary_table(workspace):
 
     # creates a csv file to store statistics
     try:
-        with open(os.path.join(workspace, file_name), 'w') as f:
-            # writes header
-            # f.write('comid,timestamp,max,mean,color,thickness,ret_per\n')
+        # writes header
+        # f.write('comid,timestamp,max,mean,color,thickness,ret_per\n')
 
-            # extracts forecast COMIDS and formatted dates into lists
-            comids = nc.Dataset(nclist[0], 'r').variables['rivid'][:].tolist()
-            rawdates = nc.Dataset(nclist[0], 'r').variables['time'][:].tolist()
-            dates = []
-            for date in rawdates:
-                dates.append(dt.datetime.utcfromtimestamp(date).strftime("%m/%d/%y %H:%M"))
-            # creates a time series of 3H intervals for interpolation,
-            # coerces a copy to appropriate type for inserting to file
-            interp_x = pd.date_range(
-                start=dates[0],
-                end=dates[-1],
-                freq='3H'
-            )
-            interp_x_strings = interp_x.strftime("%m/%d/%y %H:%M")
-            new_dates = list(interp_x_strings)
-            dt_dates = pd.to_datetime(dates)
+        # extracts forecast COMIDS and formatted dates into lists
+        comids = nc.Dataset(nclist[0], 'r').variables['rivid'][:].tolist()
+        rawdates = nc.Dataset(nclist[0], 'r').variables['time'][:].tolist()
+        dates = []
+        for date in rawdates:
+            dates.append(dt.datetime.utcfromtimestamp(date).strftime("%m/%d/%y %H:%M"))
+        # creates a time series of 3H intervals for interpolation,
+        # coerces a copy to appropriate type for inserting to file
+        interp_x = pd.date_range(
+            start=dates[0],
+            end=dates[-1],
+            freq='3H'
+        )
+        interp_x_strings = interp_x.strftime("%m/%d/%y %H:%M")
+        new_dates = list(interp_x_strings)
+        dt_dates = pd.to_datetime(dates)
 
-            # creates empty lists with forecast stats
-            maxlist = []
-            meanlist = []
+        # creates empty lists with forecast stats
+        maxlist = []
+        meanlist = []
 
-            # loops through the stat netcdf files to populate lists created above
-            for ncfile in sorted(nclist):
-                res = nc.Dataset(ncfile, 'r')
+        # loops through the stat netcdf files to populate lists created above
+        for ncfile in sorted(nclist):
+            res = nc.Dataset(ncfile, 'r')
 
-                # loops through COMIDs with netcdf files, adds maxes and means
-                # originally took a slice of the first 49 to only include high res,
-                # but now extracts the whole forecast so the interpolator can fill
-                # in the gaps.
-                for index, comid in enumerate(comids):
-                    if 'max' in ncfile:
-                        maxlist.append(res.variables['Qout'][index, :].tolist())
-                    elif 'avg' in ncfile:
-                        meanlist.append(res.variables['Qout'][index, :].tolist())
-            # loops through the lists of max lists and mean lists to interpolate using the dates as x values
-            for index, maxes, means in enumerate(zip(maxlist, meanlist)):
-                max_interpolator = pchip(dt_dates, maxes)
-                mean_interpolator = pchip(dt_dates, means)
-                int_max = max_interpolator(interp_x)
-                int_mean = mean_interpolator(interp_x)
-                maxlist[index] = int_max
-                meanlist[index] = int_mean
-
-            # loops through COMIDs again to add rows to csv file
+            # loops through COMIDs with netcdf files, adds maxes and means
+            # originally took a slice of the first 49 to only include high res,
+            # but now extracts the whole forecast so the interpolator can fill
+            # in the gaps.
             for index, comid in enumerate(comids):
-                for f_date, f_max, f_mean in zip(new_dates, maxlist[index], meanlist[index]):
-                    # define reach color based on return periods
-                    if f_mean > rp_df.loc[comid, 'return_50']:
-                        color = 'purple'
-                    elif f_mean > rp_df.loc[comid, 'return_10']:
-                        color = 'red'
-                    elif f_mean > rp_df.loc[comid, 'return_2']:
-                        color = 'yellow'
-                    else:
-                        color = 'blue'
+                if 'max' in ncfile:
+                    maxlist.append(res.variables['Qout'][index, :].tolist())
+                elif 'avg' in ncfile:
+                    meanlist.append(res.variables['Qout'][index, :].tolist())
+        # loops through the lists of max lists and mean lists to interpolate using the dates as x values
 
-                    # define reach thickness based on flow magnitude
-                    if f_mean < 20:
-                        thickness = '1'
-                    elif 20 <= f_mean < 250:
-                        thickness = '2'
-                    elif 250 <= f_mean < 1500:
-                        thickness = '3'
-                    elif 1500 <= f_mean < 10000:
-                        thickness = '4'
-                    elif 10000 <= f_mean < 30000:
-                        thickness = '5'
-                    else:
-                        thickness = '6'
+        columns = ['comid', 'timestamp', 'max', 'mean', 'color', 'thickness', 'ret_per']
+        summary_table_df = pd.DataFrame(columns=columns)
+        for index, comid, date, maxes, means in enumerate(zip(comids, new_dates, maxlist, meanlist)):
+            # interpolate maxes and means
+            max_interpolator = pchip(dt_dates, maxes)
+            mean_interpolator = pchip(dt_dates, means)
+            int_max = max_interpolator(interp_x)
+            int_mean = mean_interpolator(interp_x)
+            # loop for creating color, thickness, and return period columns
+            colors = []
+            thicknesses = []
+            ret_pers = []
+            for f_mean in int_mean:
+                # define reach color based on return periods
+                if f_mean > rp_df.loc[comid, 'return_50']:
+                    color = 'purple'
+                elif f_mean > rp_df.loc[comid, 'return_10']:
+                    color = 'red'
+                elif f_mean > rp_df.loc[comid, 'return_2']:
+                    color = 'yellow'
+                else:
+                    color = 'blue'
 
-                    # define return period exceeded by the mean forecast
-                    if f_mean > rp_df.loc[comid, 'return_100']:
-                        ret_per = '100'
-                    elif f_mean > rp_df.loc[comid, 'return_50']:
-                        ret_per = '50'
-                    elif f_mean > rp_df.loc[comid, 'return_25']:
-                        ret_per = '25'
-                    elif f_mean > rp_df.loc[comid, 'return_10']:
-                        ret_per = '10'
-                    elif f_mean > rp_df.loc[comid, 'return_5']:
-                        ret_per = '5'
-                    elif f_mean > rp_df.loc[comid, 'return_2']:
-                        ret_per = '2'
-                    else:
-                        ret_per = '0'
-                f.write(','.join([str(comid), f_date, str(f_max), str(f_mean), color, thickness, ret_per + '\n']))
-        f.close()
+                colors.append(color)
+
+                # define reach thickness based on flow magnitude
+                if f_mean < 20:
+                    thickness = '1'
+                elif 20 <= f_mean < 250:
+                    thickness = '2'
+                elif 250 <= f_mean < 1500:
+                    thickness = '3'
+                elif 1500 <= f_mean < 10000:
+                    thickness = '4'
+                elif 10000 <= f_mean < 30000:
+                    thickness = '5'
+                else:
+                    thickness = '6'
+
+                thicknesses.append(thickness)
+
+                # define return period exceeded by the mean forecast
+                if f_mean > rp_df.loc[comid, 'return_100']:
+                    ret_per = '100'
+                elif f_mean > rp_df.loc[comid, 'return_50']:
+                    ret_per = '50'
+                elif f_mean > rp_df.loc[comid, 'return_25']:
+                    ret_per = '25'
+                elif f_mean > rp_df.loc[comid, 'return_10']:
+                    ret_per = '10'
+                elif f_mean > rp_df.loc[comid, 'return_5']:
+                    ret_per = '5'
+                elif f_mean > rp_df.loc[comid, 'return_2']:
+                    ret_per = '2'
+                else:
+                    ret_per = '0'
+
+                ret_pers.append(ret_per)
+
+            # create the dataframe for the site and concatenate onto
+            site_summary = pd.DataFrame(
+                data={
+                    columns[0]: pd.Series([comid for comid in range(len(new_dates))]),
+                    columns[1]: pd.Series(new_dates),
+                    columns[2]: pd.Series(int_max),
+                    columns[3]: pd.Series(int_mean),
+                    columns[4]: pd.Series(colors),
+                    columns[5]: pd.Series(thicknesses),
+                    columns[6]: pd.Series(ret_pers)
+                }
+            )
+            summary_table_df = pd.concat([summary_table_df, site_summary], ignore_index=True)
+        # write to csv
+        summary_table_df.to_csv(os.path.join(workspace, file_name), index=False)
+
         return 'Stat Success'
     except Exception as e:
-        f.close()
         logging.debug(e)
-        print(e)
 
 
 # runs function on file execution
